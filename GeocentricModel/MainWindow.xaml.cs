b6eb7f71745d9cdebc1e168488e5761ff9403f72
Nathan Woltman <nwoltman@outlook.com>
2014-01-19 03:13:20 -0500

    Major improvements: - Moved the solution file outside of the project directory - Changed variable names to follow C# naming standards (as suggested by ReSharper) - Simplified code and improved performance - Changed image names and added an icon

--- GeocentricModel/MainWindow.xaml.cs
+++ GeocentricModel/MainWindow.xaml.cs
@@ -1,44 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Windows.Threading;
+﻿using System.Windows;
 
 namespace GeocentricModel
 {
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window
+	public partial class MainWindow
 	{
-		public static Dispatcher s_Dispatcher;
-
-		//OrbiterWithChild orbiterEarth;
-		Orbiter.OrbiterWithChild orbiterEarth;
+		Orbiter.OrbiterWithChild _orbiterEarth;
 		
 		public MainWindow()
 		{
 			InitializeComponent();
-			this.Loaded += new RoutedEventHandler(MainWindow_Loaded);
-			s_Dispatcher = this.Dispatcher;
 		}
 
 		void MainWindow_Loaded(object sender, RoutedEventArgs e)
 		{
-			double dCenterCoordinate = (Canvas1.ActualHeight > Canvas1.ActualWidth ? Canvas1.ActualWidth / 2 : Canvas1.ActualHeight / 2);
-			Point center = new Point(dCenterCoordinate, dCenterCoordinate);
-			//orbiterEarth = new Orbiter(this, ellipseOrbit, center, dCenterCoordinate - ellipseOrbit.Width / 2 - 50, 350000);
-			orbiterEarth = new Orbiter.OrbiterWithChild(Canvas1, ellipseOrbit, center, dCenterCoordinate - ellipseOrbit.Width / 2 - 50, 80000, Canvas2, ellipseOrbitOrbiter);
-			orbiterEarth.StartOrbiting();
+			double dCenterCoordinate = Canvas.ActualHeight > Canvas.ActualWidth ? Canvas.ActualWidth/2 : Canvas.ActualHeight/2;
+			var center = new Point(dCenterCoordinate, dCenterCoordinate);
+			double orbitRadius = dCenterCoordinate - ParentOrbiter.Width/2 - 50;
+			_orbiterEarth = new Orbiter.OrbiterWithChild(
+				Dispatcher,
+				Canvas,
+				ParentOrbiter,
+				center,
+				orbitRadius,
+				ChildOrbiter,
+				6);
+			_orbiterEarth.StartOrbiting();
 		}
 	}
 }
--- GeocentricModel/Orbiter.cs
+++ GeocentricModel/Orbiter.cs
@@ -1,137 +1,141 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Shapes;
 using System.Windows.Threading;
-using System.Timers;
 
 namespace GeocentricModel
 {
 	public class Orbiter
 	{
 		//Member variables
-		public Point Center { get { return getPosition(); } private set { setPosition(value); } }
+		public Point Center
+		{
+			get { return GetPosition(); }
+			private set { SetPosition(value); }
+		}
 		public double Radius { get; private set; }
 		public double OrbitRadius { get; private set; }
 		public Brush OrbitColour { get; private set; }
 		public Point RotationCenter { get; private set; }
 
 		protected DispatcherTimer Timer;
-		protected double THETA_DECREMENTER;
-		
-		Ellipse Circle;
-		double Theta = 360;
-		Canvas Canvas;
+		protected double ThetaDecrementer = 2.0;
+
+		private double _theta = 360.0;
+		private readonly Canvas _canvas;
+		private readonly Ellipse _circle;
+		private const int MAX_DOTS = 5000;
 
 
 		//Constructor
-		public Orbiter(Canvas canvas, Ellipse ellipse, Point rotationCenter, double orbitRadius, long ticks)
+		public Orbiter(Dispatcher dispatcher, Canvas canvas, Ellipse ellipse, Point rotationCenter, double orbitRadius)
 		{
-			Debug.Assert(ellipse.Width == ellipse.Height, "Input Ellipse is not a circle");
-			if (ellipse.Width != ellipse.Height) return;
-
-			Circle = ellipse;
-			Radius = (int) Circle.Width / 2;	
-
+			Debug.Assert(ellipse.Width == ellipse.Height, "Input Ellipse is not a circle!");
+			
+			//Configure the orbiter
+			_circle = ellipse;
+			Radius = _circle.Width / 2;	
 			RotationCenter = rotationCenter;
 			OrbitRadius = orbitRadius;
-
 			OrbitColour = ellipse.Stroke;
 
 			//Configure the timer
-			Timer = new DispatcherTimer(DispatcherPriority.Normal, MainWindow.s_Dispatcher);
-			Timer.Interval = new TimeSpan(ticks);
+			Timer = new DispatcherTimer(DispatcherPriority.Normal, dispatcher)
+			{
+				Interval = new TimeSpan(0, 0, 0, 0, 16) //Timer ticks every 16 ms
+			};
 			Timer.Tick += Timer_Tick;
 
-			THETA_DECREMENTER = 1;
-
-			Canvas = canvas;
+			//Set the canvas to draw on
+			_canvas = canvas;
 		}
-
-		//===================================================
-		// Member functions
-		//===================================================
 
 		public void StartOrbiting()
 		{
 			Timer.Start();
 		}
 
-		Point getPosition()
+		Point GetPosition()
 		{
-			return new Point(Circle.Margin.Left + Radius, Circle.Margin.Top + Radius);
+			return new Point(_circle.Margin.Left + Radius, _circle.Margin.Top + Radius);
 		}
 
-		protected virtual void setPosition(Point point)
+		protected virtual void SetPosition(Point point)
 		{
-			Circle.Margin = new Thickness(point.X - Radius, point.Y - Radius, 0, 0);
+			_circle.Margin = new Thickness(point.X - Radius, point.Y - Radius, 0, 0);
 		}
 
 		protected virtual void Timer_Tick(object sender, EventArgs e)
 		{
-			if (Theta == 0) 
-				Theta = 360; //To prevent Theta from getting too small
+			//Prevent Theta from getting too small
+			if (_theta < 0) 
+				_theta += 360;
 
-			double Radians = Math.PI * Theta / 180; //Calculate radians from degress
 			//Set the new position of the orbiter
-			Center = new Point(RotationCenter.X + Math.Cos(Radians) * OrbitRadius, RotationCenter.Y + Math.Sin(Radians) * OrbitRadius);
-			Theta -= THETA_DECREMENTER; //Decrement the angle theta
+			double radians = Math.PI * _theta / 180; //Calculate radians from degress
+			Center = new Point(RotationCenter.X + Math.Cos(radians) * OrbitRadius, RotationCenter.Y + Math.Sin(radians) * OrbitRadius);
 
-			DrawPoint();
+			_theta -= ThetaDecrementer; //Decrement the angle theta
+			DrawDot(); //Draw a new dot to track the orbiter's progress
 		}
 
-		void DrawPoint()
+		private void DrawDot()
 		{
-			Ellipse dot = new Ellipse();
-			dot.Width = 1;
-			dot.Height = 1;
-			dot.Margin = new Thickness(Center.X, Center.Y, 0, 0);
-			dot.Fill = OrbitColour;
-
-			if (Canvas.Children.Count < 5000)
+			var dot = new Ellipse
 			{
-				//Only add the dot if there are currently less than 5000 dots already drawn
-				Canvas.Children.Add(dot);
-			}
+				Width = 1,
+				Height = 1,
+				Margin = new Thickness(Center.X, Center.Y, 0, 0),
+				Fill = OrbitColour
+			};
+
+			//Only add the dot if there is currently less than the maximum number of dots already drawn
+			if (_canvas.Children.Count < MAX_DOTS)
+				_canvas.Children.Add(dot);
 		}
 
 
 		#region OrbiterWithChild
+		/// <summary>
+		/// A class for an Orbiter with another Orbiter orbiting around it
+		/// </summary>
 		public class OrbiterWithChild : Orbiter
 		{
-			Orbiter Child;
-			int TickCounter = 2;
-			Point CenterReference = new Point();
+			private readonly Orbiter _child;
+			private readonly Point _centerReference = new Point();
 
-			public OrbiterWithChild(Canvas canvas, Ellipse ellipse, Point rotationCenter, double orbitRadius, long ticks, Canvas childCanvas, Ellipse child)
-				: base(canvas, ellipse, rotationCenter, orbitRadius, ticks)
+			/// <summary>
+			/// Constructor for an OrbiterWithOrbiter
+			/// </summary>
+			/// <param name="dispatcher">The Dispatcher that will dispatch timer events</param>
+			/// <param name="canvas">The Canvas to draw on</param>
+			/// <param name="parent">The ellipse representing the parent Orbiter</param>
+			/// <param name="rotationCenter">The Point that the parent Orbiter rotates around</param>
+			/// <param name="orbitRadius">The radius of the parent Orbiter</param>
+			/// <param name="child">The ellipse represeting the child Orbiter</param>
+			/// <param name="speed">Indicates how many times faster the child orbits the parent than the parent orbits its center</param>
+			public OrbiterWithChild(Dispatcher dispatcher, Canvas canvas, Ellipse parent, Point rotationCenter, double orbitRadius, Ellipse child, int speed)
+				: base(dispatcher, canvas, parent, rotationCenter, orbitRadius)
 			{
-				Child = new Orbiter(childCanvas, child, CenterReference, this.Radius, ticks / 3);
-				THETA_DECREMENTER = 0.5;
+				_child = new Orbiter(dispatcher, canvas, child, _centerReference, Radius);
+				ThetaDecrementer = ThetaDecrementer/speed;
 			}
 
-			protected override void setPosition(Point point)
+			protected override void SetPosition(Point point)
 			{
-				base.setPosition(point);
-				CenterReference.X = this.Center.X;
-				CenterReference.Y = this.Center.Y;
+				base.SetPosition(point);
+				//Update this (parent) Orbiter's center reference for the child to orbit around
+				_centerReference.X = Center.X;
+				_centerReference.Y = Center.Y;
 			}
 
 			protected override void Timer_Tick(object sender, EventArgs e)
 			{
-				Timer.Stop();
-				TickCounter++;
-				//Only orbit this orbiter every 2 ticks
-				if (TickCounter > 2)
-				{
-					base.Timer_Tick(sender, e);
-					TickCounter = 0;
-				}
-				Child.Timer_Tick(sender, e); //Make the child orbit
-				Timer.Start();
+				base.Timer_Tick(sender, e); //Make this (parent) Orbiter orbit
+				_child.Timer_Tick(sender, e); //Make the child orbit
 			}
 
 		}
